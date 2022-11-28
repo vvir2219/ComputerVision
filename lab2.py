32e1fe2b53832d8e1e7083d2b9a6e3cd7b1ed337
Vlad Vitca <vilie0215@gmail.com>
2022-11-28 22:09:52 +0200

    color

--- lab2.py
+++ lab2.py
@@ -9,69 +9,61 @@ import platform
 print(cv.__version__)
 print(platform.architecture())
 
+def static_vars(**kwargs):
+    def decorate(func):
+        for k in kwargs:
+            setattr(func, k, kwargs[k])
+        return func
+    return decorate
+
+def floodFill(image, showImage):
+    kernel = np.ones((5,5),np.uint8)
+    grayimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    contour = cv.morphologyEx(grayimg, cv.MORPH_GRADIENT, kernel)
+    ret, thresh = cv.threshold(contour, 15, 255, cv.THRESH_BINARY)
+    contour = ~cv.morphologyEx(thresh, cv.MORPH_CLOSE, kernel)
+
+    #  cv.imshow('contour', contour)
+
+    fillmorph = cv.getStructuringElement(cv.MORPH_CROSS,(3,3))
+
+    @static_vars(pick_color=True, picked_color=[255, 255, 255])
+    def handler(event, x, y, flags, param):
+        nonlocal fillmorph, image, contour
+
+        if event == cv.EVENT_LBUTTONDOWN:
+            if (handler.pick_color):
+                handler.pick_color = False
+                handler.picked_color = image[y][x]
+            else:
+                fill = np.zeros_like(contour)
+                fill[y][x] = 255
+                while True:
+                    new_fill = cv.dilate(fill, fillmorph) & contour
+                    if np.array_equal(new_fill, fill):
+                        break
+                    else:
+                        fill = new_fill
+
+                B = np.vectorize(lambda x: handler.picked_color[0] if x > 0 else np.uint8(0))
+                G = np.vectorize(lambda x: handler.picked_color[1] if x > 0 else np.uint8(0))
+                R = np.vectorize(lambda x: handler.picked_color[2] if x > 0 else np.uint8(0))
+
+                image |= cv.merge((B(fill), G(fill), R(fill)))
+                showImage()
+
+                handler.picked_color = False
+
+    showImage()
+    return handler
+
 img = cv.imread('./rocket.jpg', 1)
 
-def mouseHandler(image):
-    def handler(event, x, y, flags, param):
-        if event == cv.EVENT_LBUTTONDOWN:
-            print(x, y)
-
-    return handler
-
 cv.namedWindow('image')
-cv.setMouseCallback('image', mouseHandler(img))
-
-kernel = np.ones((3,3),np.uint8)
-grayimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-#  cv.imshow('grayimg', grayimg)
-
-#  thr = cv.adaptiveThreshold(grayimg, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
-#  cv.imshow('thr', thr)
-#  
-#  closing = cv.morphologyEx(thr, cv.MORPH_CLOSE, kernel)
-#  cv.imshow('closing', closing)
-#  
-#  erode = cv.erode(thr, kernel, iterations=1)
-#  cv.imshow('erode', erode)
-
-contour = cv.dilate(grayimg, np.ones((5,5), np.uint8), iterations=1) - \
-          cv.erode(grayimg, np.ones((5,5), np.uint8), iterations=1)
-#  cv.imshow('contour', contour)
-
-ret, thresh = cv.threshold(contour, 15, 255, cv.THRESH_BINARY)
-#  thres = cv.adaptiveThreshold(contour, 30, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 19, 7)
-
-
-def floodFill(image):
-    kernel = cv.getStructuringElement(cv.MORPH_CROSS,(3,3))
-
-    def handler(event, x, y, flags, param):
-        nonlocal kernel, image
-
-        if event == cv.EVENT_LBUTTONDOWN:
-            fill = np.zeros_like(image)
-            fill[y][x] = 255
-            while True:
-                new_fill = cv.dilate(fill, kernel) & ~image
-                if np.array_equal(new_fill, fill):
-                    break
-                else:
-                    fill = new_fill
-
-            image |= fill
-            print(x, y)
-
-    return handler
-
-contour = cv.morphologyEx(thresh, cv.MORPH_CLOSE, kernel)
-
-cv.namedWindow('contour')
-cv.setMouseCallback('contour', floodFill(contour))
+cv.setMouseCallback('image', floodFill(img, lambda: cv.imshow('image', img)))
 
 while(1):
-    cv.imshow('contour',contour)
     k = cv.waitKey(0) & 0xFF
-
     if k == ord('q') or k == 27:
         break
 
