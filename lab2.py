d1efe04bdeb84c194aa081f277c2643b6b07f035
Vlad Vitca <vilie0215@gmail.com>
2022-12-07 14:24:33 +0200

    fill with color

--- lab2.py
+++ lab2.py
@@ -17,13 +17,18 @@ def static_vars(**kwargs):
     return decorate
 
 def floodFill(image, showImage):
-    kernel = np.ones((5,5),np.uint8)
-    grayimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    contour = cv.morphologyEx(grayimg, cv.MORPH_GRADIENT, kernel)
-    ret, thresh = cv.threshold(contour, 15, 255, cv.THRESH_BINARY)
-    contour = ~cv.morphologyEx(thresh, cv.MORPH_CLOSE, kernel)
+    contour = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    contour = cv.erode(contour, np.ones((3, 3)), iterations=2)
+    contour = cv.morphologyEx(contour, cv.MORPH_GRADIENT, np.ones((4, 4)))
+    ret, contour = cv.threshold(contour, 15, 255, cv.THRESH_BINARY)
+    #  contour = cv.morphologyEx(contour, cv.MORPH_CLOSE, np.ones((5, 5)))
+    #  contour = ~cv.adaptiveThreshold(contour,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY,3,2)
+    #  contour = cv.dilate(contour, np.ones((3, 3)))
+    #  contour = cv.erode(contour, np.ones((3, 3)))
+    #  contour = cv.morphologyEx(contour, cv.MORPH_CLOSE, np.ones((3, 3)))
+    contour = ~contour
 
-    #  cv.imshow('contour', contour)
+    cv.imshow('contour', contour)
 
     fillmorph = cv.getStructuringElement(cv.MORPH_CROSS,(3,3))
 
@@ -35,30 +40,34 @@ def floodFill(image, showImage):
             if (handler.pick_color):
                 handler.pick_color = False
                 handler.picked_color = image[y][x]
+                print("Picked color: ", handler.picked_color)
             else:
-                fill = np.zeros_like(contour)
-                fill[y][x] = 255
+                print("Filling with color")
+                mask = np.zeros_like(contour)
+                mask[y][x] = 1
                 while True:
-                    new_fill = cv.dilate(fill, fillmorph) & contour
-                    if np.array_equal(new_fill, fill):
+                    new_fill = cv.dilate(mask, fillmorph) & contour
+                    if np.array_equal(new_fill, mask):
                         break
                     else:
-                        fill = new_fill
+                        mask = new_fill
 
-                B = np.vectorize(lambda x: handler.picked_color[0] if x > 0 else np.uint8(0))
-                G = np.vectorize(lambda x: handler.picked_color[1] if x > 0 else np.uint8(0))
-                R = np.vectorize(lambda x: handler.picked_color[2] if x > 0 else np.uint8(0))
+                reverse_mask = ((1 - cv.merge((mask, mask, mask)))*(-1)).astype(np.uint8)
+                colored_mask = cv.merge((mask*handler.picked_color[0], mask*handler.picked_color[1], mask*handler.picked_color[2]))
+                image = (image & reverse_mask) + colored_mask
+                print("Filled with color")
+                #  cv.imshow('colored_mask', colored_mask)
 
-                image |= cv.merge((B(fill), G(fill), R(fill)))
-                showImage()
+                showImage(image)
+                handler.pick_color = True
 
-    showImage()
+    showImage(image)
     return handler
 
 img = cv.imread('./rocket.jpg', 1)
 
 cv.namedWindow('image')
-cv.setMouseCallback('image', floodFill(img, lambda: cv.imshow('image', img)))
+cv.setMouseCallback('image', floodFill(img, lambda image: cv.imshow('image', image)))
 
 while(1):
     k = cv.waitKey(0) & 0xFF
